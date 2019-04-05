3a4493882576dc0e567f1dfc5354b67843e5cced
JR Tashjian <jrtashjian@gmail.com>
2019-04-05 00:25:21 -0400

    Updating block icon.

--- assets/src/block.js
+++ assets/src/block.js
@@ -2,15 +2,14 @@
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const {
-    ServerSideRender, PanelBody, SVG, Path,
+    ServerSideRender, PanelBody, ExternalLink,
     RangeControl, ToggleControl, SelectControl,
-    ExternalLink,
 } = wp.components;
 const { withSelect } = wp.data;
 const { InspectorControls, RichText } = wp.editor;
 const { Fragment } = wp.element;
 
-const icon = <SVG viewBox="0 0 24 24"><Path d="M16,19 L16,20 C16,21.1045695 15.1045695,22 14,22 L4,22 C2.8954305,22 2,21.1045695 2,20 L2,10 C2,8.8954305 2.8954305,8 4,8 L5,8 L5,7 C5,5.8954305 5.8954305,5 7,5 L8,5 L8,4 C8,2.8954305 8.8954305,2 10,2 L20,2 C21.1045695,2 22,2.8954305 22,4 L22,14 C22,15.1045695 21.1045695,16 20,16 L19,16 L19,17 C19,18.1045695 18.1045695,19 17,19 L16,19 Z M16,17 L17,17 L17,7 L7,7 L7,8 L14,8 C15.1045695,8 16,8.8954305 16,10 L16,17 Z M19,14 L20,14 L20,4 L10,4 L10,5 L17,5 C18.1045695,5 19,5.8954305 19,7 L19,14 Z M4,10 L4,20 L14,20 L14,10 L4,10 Z M5,11 L13,11 L13,13 L5,13 L5,11 Z M5,14 L13,14 L13,16 L5,16 L5,14 Z M5,17 L13,17 L13,19 L5,19 L5,17 Z"></Path></SVG>;
+const icon = <svg viewbox="0 0 24 24"><g fill="none" fill-rule="evenodd"><rect fill="#FFD700" width="24" height="24" rx="2" /><path d="M18 16l-2 1-7-4v-1-1l7-4a3 3 0 0 0 5-2 3 3 0 1 0-6 1l-7 4a3 3 0 0 0-5 2 3 3 0 0 0 5 2l7 4v1a3 3 0 1 0 3-3z" fill="#191500" fill-rule="nonzero" /></g></svg>;
 
 registerBlockType('curatewp/related-posts', {
     title: __('Related Posts', 'cwprp'),
