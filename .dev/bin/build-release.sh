0007d11da61e2fbaa8f6e3280c25cd5bcda5d72f
JR Tashjian <jrtashjian@gmail.com>
2019-10-06 01:38:33 -0400

    Add new release script

---
+++ .dev/bin/build-release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PLUGIN="curatewp-related-posts"
+
+WORKING_DIR=`pwd`
+
+mkdir -p release/$PLUGIN
+git clone https://github.com/jrtashjian/${PLUGIN} release/repo
+
+cd release/repo
+composer install --no-dev
+npm install && npm run build
+cd $WORKING_DIR
+
+rsync -av --progress --exclude={'.*','wordpress','node_modules','src','release','.gitignore','composer.*','package*','phpcs.xml','README.md'} release/repo/* release/$PLUGIN
